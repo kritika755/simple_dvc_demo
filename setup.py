1f6ebe7d287b33c0748947498abbcf389e919e41
kritika755 <kritiikaaa@gmail.com>
2022-09-23 20:30:13 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    description="its a wineq package",
+    author="kritika",
+    packages=find_packages(),
+    license="MIT License",
+
+)
